4d22023f2fdbd127b93cbde250d93f6ce27c0ec7
Teilor Souza Barcelos <teilor.barcelos@uds.com.br>
2023-03-20 20:05:04 -0300

    boilerplate done :tada:

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,36 @@
 import "@testing-library/jest-dom/extend-expect";
 import { mswServer } from "./src/mocks/server";
+import { useRouter } from 'next/router';
+
+jest.mock('next/router', () => ({
+  useRouter: jest.fn().mockReturnValue({
+      push: jest.fn(),
+      route: '',
+      pathname: '',
+      query: {},
+      asPath: '',
+      basePath: '',
+      isLocaleDomain: false,
+      isFallback: false,
+      replace: jest.fn(),
+      reload: jest.fn(),
+      back: jest.fn(),
+      forward: jest.fn(),
+      prefetch: jest.fn(),
+      beforePopState: jest.fn(),
+      events: {
+        on: jest.fn(),
+        off: jest.fn(),
+        emit: jest.fn(),
+      },
+      isReady: true,
+      isPreview: false,
+    })
+}));
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
 
 beforeAll(() => mswServer.listen());
 afterEach(() => mswServer.resetHandlers());
---
+++ src/pages/login/login.test.tsx
@@ -0,0 +1,16 @@
+import { render, fireEvent, waitFor } from "@testing-library/react";
+import { useAuthStore } from "@/lib/auth";
+import Login from ".";
+
+describe("Login component", () => {
+  it("should redirect to home page after login", async () => {
+    const setStateSpy = jest.spyOn(useAuthStore, "setState");
+    
+    const { getByRole } = render(<Login />);
+    const loginButton = getByRole("button", { name: "Login" });
+
+    fireEvent.click(loginButton);
+
+    await waitFor(() => expect(setStateSpy).toBeCalledWith({ loggedIn: true }));
+  });
+});
