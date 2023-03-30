07b5536fbe0ee15e8a41e0abe3fa078a76821bf0
Teilor Souza Barcelos <teilor.barcelos@uds.com.br>
2023-03-30 00:19:47 -0300

    feat: Msw server runing on development to simulate api response

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,11 +6,14 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { Loading } from "@/components/Loading";
 import { useLoadingStore } from "@/lib/contexts/loading";
+import { mswServer } from "@/mocks/server";
 
 export default function App({ Component, pageProps }: AppProps) {
   const { isLoading } = useLoadingStore();
   const router = useRouter();
   const { loggedIn } = useAuthStore();
+
+  if (process.env.NODE_ENV !== "production") mswServer.listen();
 
   useEffect(() => {
     if (!loggedIn && router.asPath !== "/login") {
